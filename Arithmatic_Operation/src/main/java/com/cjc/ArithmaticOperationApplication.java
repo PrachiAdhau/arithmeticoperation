98d5121c6ae3e63e41399f112ebc8cc83aa51914
honmuteneha <honmuteneha@gmail.com>
2024-10-09 18:09:45 +0530

    project

---
+++ Arithmatic_Operation/src/main/java/com/cjc/ArithmaticOperationApplication.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmaticOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmaticOperationApplication.class, args);
+	}
+
+}
---
+++ Arithmatic_Operation/src/test/java/com/cjc/ArithmaticOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmaticOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
